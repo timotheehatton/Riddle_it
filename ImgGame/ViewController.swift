0d00c5bcde7186345ac2370deb69ea633f6ed213
timotheehatton <timothee.hatton@gmail.com>
2018-06-08 02:13:47 +0200

    :sparkles: music player

--- ImgGame/ViewController.swift
+++ ImgGame/ViewController.swift
@@ -7,42 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
     var wordsToCompare: [String] = ["AMPOULE", "ASPIRATEUR", "BETONNIERE", "BEYONCE", "BIERE", "CHEVEUX", "CLAVIER", "DENTS", "ENCEINTE", "GUITARE", "HALTERES", "JEAN", "LUNETTES", "MELON", "MICRO", "MONTRE", "NUAGE", "PIANO", "PIECE", "POISSON", "POUBELLE", "ROSE", "SCOTCH", "SKATE", "TELECOMMANDE", "VELO", "VINYLE"]
-    
+    var player: AVAudioPlayer?
     let manager = FileManager.default
-    
-    @IBAction func prepareForUnwind (sender: UIStoryboardSegue) {}
-    
-    override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
-        let segue = UnwindScaleSegue(identifier: unwindSegue.identifier, source: unwindSegue.source, destination: unwindSegue.destination)
-        segue.perform()
-    }
-    
-    @IBOutlet weak var image: UIImageView!
-    
-    @IBOutlet weak var levelLabel: CustomButton!
-    
-    @IBOutlet weak var numberOfTriesLabel: UILabel!
-    
     var level = 1
     var numberOfTries = 0
     
-    override func viewDidLoad()
+    @IBAction func prepareForUnwind (sender: UIStoryboardSegue) {}
+    override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController)
     {
-        super.viewDidLoad()
+        let segue = UnwindScaleSegue(identifier: unwindSegue.identifier, source: unwindSegue.source, destination: unwindSegue.destination)
+        segue.perform()
     }
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var levelLabel: CustomButton!
+    @IBOutlet weak var numberOfTriesLabel: UILabel!
+    @IBOutlet weak var musicButtonInput: UIButton!
     
     override func viewDidAppear(_ animated: Bool)
     {
         initView()
     }
     
+    override func viewDidLoad()
+    {
+        playSound()
+        super.viewDidLoad()
+    }
+    
     func initView()
     {
         guard let url = self.manager.urls(for: .documentDirectory, in: .allDomainsMask).first else { fatalError() }
-        print(url)
         let documentUrl = url.appendingPathComponent("game-data.json")
         
         let dataRead = try? Data(contentsOf: documentUrl)
@@ -65,5 +64,36 @@ class ViewController: UIViewController {
         numberOfTriesLabel.text = String(numberOfTries)
         
         image.image = UIImage(named: "\(wordsToCompare[level - 1].lowercased())-min.jpg")
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "music", withExtension: "mp3") else { return }
+        print(url)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            print(player)
+            
+            player.numberOfLoops = -1
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func musicButton(_ sender: Any) {
+        if player!.isPlaying {
+            player?.pause()
+            musicButtonInput.alpha = 0.5
+        }  else {
+            playSound()
+            musicButtonInput.alpha = 1
+        }
     }
 }
